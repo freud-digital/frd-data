ec4f4fee77f7a5c968f79d379940d0cf2dd2f4c5
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-15 11:49:11 +0200

    change path for saved output

--- collate_work.py
+++ collate_work.py
@@ -9,7 +9,7 @@ from config import WERK_PATH
 input_glob = f"./werke/{WERK_PATH}/{WERK_PATH}__*.xml"
 output_dir = f"./werke/{WERK_PATH}/collated"
 werk_path = "{WERK_PATH}"
-
+print("starting...")
 out = CxCollate(
     glob_pattern=input_glob,
     glob_recursive=False,
@@ -22,7 +22,7 @@ out = CxCollate(
 files = glob.glob(f"{output_dir}/*.tei")
 print(len(files))
 full_doc = merge_tei_fragments(files)
-with open(f'{WERK_PATH}.xml', 'w') as f:
+with open(f'{output_dir}/{WERK_PATH}.xml', 'w') as f:
     f.write(ET.tostring(full_doc, encoding='UTF-8').decode('utf-8'))
 
 
