8a2f0419a8c65788561bec04cb3827d893a16d6f
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-15 14:05:29 +0200

    valid tei

--- collate_work.py
+++ collate_work.py
@@ -2,13 +2,17 @@ import glob
 import os
 import lxml.etree as ET
 from acdh_collatex_utils.acdh_collatex_utils import CxCollate
-from acdh_collatex_utils.post_process import merge_tei_fragments
+from acdh_collatex_utils.post_process import (
+    merge_tei_fragments,
+    make_full_tei_doc
+)
 
 from config import WERK_PATH
 
 input_glob = f"./werke/{WERK_PATH}/{WERK_PATH}__*.xml"
 output_dir = f"./werke/{WERK_PATH}/collated"
-werk_path = "{WERK_PATH}"
+result_file = f'{output_dir}/{WERK_PATH}.xml'
+
 print("starting...")
 out = CxCollate(
     glob_pattern=input_glob,
@@ -18,12 +22,14 @@ out = CxCollate(
     chunk_size=5000,
 ).collate()
 
-
 files = glob.glob(f"{output_dir}/*.tei")
 print(len(files))
 full_doc = merge_tei_fragments(files)
-with open(f'{output_dir}/{WERK_PATH}.xml', 'w') as f:
+with open(result_file, 'w') as f:
     f.write(ET.tostring(full_doc, encoding='UTF-8').decode('utf-8'))
+full_tei = make_full_tei_doc(result_file)
+root = full_tei.tree
+full_tei.tree_to_file(result_file)
 
 
 for x in files:
