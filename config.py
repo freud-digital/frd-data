c5ace7bb1d4457951fa6074a1bf1f0d5f8cacbbe
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-15 09:40:03 +0200

    local download script/wf added

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+FRD_USER = os.environ.get('FRD_USER')
+FRD_PW = os.environ.get('FRD_PW')
+WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
+WERK_PATH = "uber-den-traum"
---
+++ download_work.py
@@ -0,0 +1,34 @@
+import click
+import freud_api_crawler.freud_api_crawler as frd
+from config import WERK_ID, FRD_USER, FRD_PW
+
+
+out_dir = "."
+
+
+auth_items = frd.get_auth_items(FRD_USER, FRD_PW)
+werk_obj = frd.FrdWerk(
+    auth_items=auth_items, werk_id=WERK_ID
+)
+rel_manifestations = werk_obj.manifestations
+for x in rel_manifestations:
+    try:
+        frd_man = frd.FrdManifestation(
+            out_dir=out_dir,
+            manifestation_id=x['man_id'],
+            auth_items=auth_items
+        )
+        frd_man.make_xml(save=True, limit=False)
+    except Exception as e:
+        click.echo(
+            click.style(
+                f"processing Manifestation {x} did not not work due to Error {e}",
+                fg='red'
+            )
+        )
+click.echo(
+    click.style(
+        f"finished download\n{werk_obj.manifestations_count} Manifestations for {werk_obj.md__title}",
+        fg='green'
+    )
+)
