91bc4202190862d2377fcc521b816e8af85887ad
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-15 15:14:04 +0200

    WERK_ID = "0e8b07dc-123f-4554-8139-c7b15a75aaaf" WERK_PATH = "jenseits-des-lustprinzips"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "71352766-fe18-4d78-860e-25e26d0f78d5"
-WERK_PATH = "rauchgewohnheiten"
+WERK_ID = "0e8b07dc-123f-4554-8139-c7b15a75aaaf"
+WERK_PATH = "jenseits-des-lustprinzips"
