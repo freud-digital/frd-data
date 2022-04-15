672a262839d29c5257babfd4b195f072e755aacc
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-15 11:11:04 +0200

    added collation wf-file to close #1

--- collate_work.py
+++ collate_work.py
@@ -1,11 +1,12 @@
 import glob
+import os
 import lxml.etree as ET
 from acdh_collatex_utils.acdh_collatex_utils import CxCollate
 from acdh_collatex_utils.post_process import merge_tei_fragments
 
 from config import WERK_PATH
 
-input_glob = f"./werke/{WERK_PATH}/{WERK_PATH}*.xml"
+input_glob = f"./werke/{WERK_PATH}/{WERK_PATH}__*.xml"
 output_dir = f"./werke/{WERK_PATH}/collated"
 werk_path = "{WERK_PATH}"
 
@@ -21,6 +22,12 @@ out = CxCollate(
 files = glob.glob(f"{output_dir}/*.tei")
 print(len(files))
 full_doc = merge_tei_fragments(files)
-
 with open(f'{WERK_PATH}.xml', 'w') as f:
     f.write(ET.tostring(full_doc, encoding='UTF-8').decode('utf-8'))
+
+
+for x in files:
+    print(f"removing {x}")
+    os.remove(x)
+    print(f"removing {x.replace('.tei', '.html')}")
+    os.remove(x.replace('.tei', '.html'))
