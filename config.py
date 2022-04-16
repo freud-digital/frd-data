0a602252db41c002798c6b1a32c771fca379b595
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-16 14:37:38 +0200

    WERK_ID = "38f2281c-c42b-463f-aab3-66d0f1e454aa" WERK_PATH = "traum-und-telepathie"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "0e8b07dc-123f-4554-8139-c7b15a75aaaf"
-WERK_PATH = "jenseits-des-lustprinzips"
+WERK_ID = "38f2281c-c42b-463f-aab3-66d0f1e454aa"
+WERK_PATH = "traum-und-telepathie"
