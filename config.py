f43d8bc357b5cfa5c6b9d23b9e9a1b4b3c0fac12
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-16 14:40:33 +0200

    WERK_ID = "82a677b1-17c0-4633-b48e-f89016306ee3" WERK_PATH = "dr-anton-von-freund"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "38f2281c-c42b-463f-aab3-66d0f1e454aa"
-WERK_PATH = "traum-und-telepathie"
+WERK_ID = "82a677b1-17c0-4633-b48e-f89016306ee3"
+WERK_PATH = "dr-anton-von-freund"
