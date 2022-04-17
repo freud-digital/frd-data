b74acfee348ca2e88f669a14762509844f2294e1
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-17 12:36:39 +0200

    WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa" WERK_PATH = "die-freudsche-psychoanalytische-methode"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "82a677b1-17c0-4633-b48e-f89016306ee3"
-WERK_PATH = "dr-anton-von-freund"
+WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa"
+WERK_PATH = "die-freudsche-psychoanalytische-methode"
