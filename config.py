231b4c5a9ee7c50d7f3011189de28b7d01c87c2d
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-18 09:13:10 +0200

    WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c" WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa"
-WERK_PATH = "die-freudsche-psychoanalytische-methode"
+WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c"
+WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"
