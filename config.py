f3bba63ecaf5766e2f9b1d92006427249a8f444d
csae8092 <peter.andorfer@oeaw.ac.at>
2022-04-19 17:08:42 +0200

    WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e" WERK_PATH = "uber-den-traum"

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c"
-WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"
+WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
+WERK_PATH = "uber-den-traum"
