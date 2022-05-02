92ff5919b89719cc348c5e97a50c37a575bff8bb
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-02 13:59:54 +0200

    WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c" WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"
    MANIFEST_DEFAULT_FILTER = {
        "field_doc_component.id": FULL_MANIFEST,
        "field_status_umschrift": 1
    }

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
+FULL_MANIFEST = "228361d0-4cda-4805-a2f8-a05ee58119b6"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
-WERK_ID = "9d035a03-28d7-4013-adaf-63337d78ece4"
-WERK_PATH = "drei-abhandlungen-zur-sexualtheorie"
-FULL_MANIFEST = "228361d0-4cda-4805-a2f8-a05ee58119b6"
+
+WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c"
+WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
-    "field_status_umschrift": 2
+    "field_status_umschrift": 1
 }
