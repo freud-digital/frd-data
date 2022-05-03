87ec7f78b5ce4e34dd9523736fd49b1ba33e17a4
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-03 13:05:50 +0200

    uber-den-traum

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
 
 FULL_MANIFEST = "228361d0-4cda-4805-a2f8-a05ee58119b6"
+HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c"
-WERK_PATH = "josef-popper-lynkeus-und-die-theorie-des-traumes"
+WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
+WERK_PATH = "uber-den-traum"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
-    # "field_status_umschrift": 1
+    "field_manifestation_typ.id": HISTORISCHE_AUSGABE,
+    "field_status_umschrift": 2
 }
