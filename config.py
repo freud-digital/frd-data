1ab056e3ef8b2bbbc02e51017d306e1747702bb0
Peter Andorfer <peter.andorfer@oeaw.ac.at>
2022-05-09 16:02:43 +0200

    ich und es

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
-WERK_PATH = "1901-01"
+WERK_ID = "aa05f7aa-fb44-41bc-893a-e5cf0ea95255"
+WERK_PATH = "1923-005"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
