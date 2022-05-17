87125e36619b254e879829952e2c676100fdd17a
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-17 07:30:33 +0200

    use zotero key as xml:ids

--- zotero_utils.py
+++ zotero_utils.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlencode
 from acdh_tei_pyutils.tei import TeiReader
 import lxml.etree as ET
+from tqdm import tqdm
 
 
 def get_listbibl(group_id="4690432", limit="50"):
@@ -23,8 +24,11 @@ def update_listbibl(url, orig_file='./indices/listbibl.xml'):
         print(e)
         return doc
     list_bibl = orig_doc.any_xpath('.//tei:listBibl')[0]
-    for x in doc.any_xpath('.//tei:biblStruct'):
+    bibls = doc.any_xpath('.//tei:biblStruct')
+    for x in tqdm(bibls, total=len(bibls)):
         zot_id = x.attrib['corresp']
+        zot_xml_id = f"zotero__{zot_id.split('/')[-1]}"
+        x.attrib['{http://www.w3.org/XML/1998/namespace}id'] = zot_xml_id
         orig_nodes = orig_doc.any_xpath(f".//tei:biblStruct[@corresp='{zot_id}']")
         for bad in orig_nodes:
             bad.getparent().remove(bad)
