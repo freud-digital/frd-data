278606af301731f16112828401f4fc1a5dbd2e20
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-06-01 12:53:40 +0200

    fetched 1903-001/1903

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "7f1ce2ef-dc26-45e2-b90a-85d866403b1c"
-WERK_PATH = "1923-007"
-READING_WIT = "sfe-1923-007__1923.xml"
+WERK_ID = "9771658d-e34f-4b8e-9f1c-9c126e5eef01"
+WERK_PATH = "1903-001"
+READING_WIT = "sfe-1903-001__1903.xml"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
