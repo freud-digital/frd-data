46b862407b28cda07f4c09447b4922ec04a23e7b
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-06-27 11:58:42 +0200

    download of all works with latest v1.9.8 of freud-api-crawler

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa"
-WERK_PATH = "1904-002"
-READING_WIT = "sfe-1904-002__1904.xml"
+WERK_ID = "38bda5e0-b14a-4250-9379-eef0a9bae370"
+WERK_PATH = "1905-002"
+READING_WIT = "sfe-1905-002__1905.xml"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
