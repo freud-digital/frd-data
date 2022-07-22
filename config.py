7f533835321191bb3610d6f75fe66e9237d3df81
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-07-22 10:49:47 +0200

    download of works with latest api crawler 1.9.9

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "92ef542d-8b7e-451f-9db2-468e0006caa7"
-WERK_PATH = "1905-008"
-READING_WIT = "sfe-1905-008__1905.xml"
+WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa"
+WERK_PATH = "1904-002"
+# READING_WIT = "sfe-1905-008__1905.xml"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
