f26e30f5d3e89e92809442cfd95b738d7a50c698
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-08-09 15:32:09 +0200

    update api crawler to v2.0.0; config set to 1901-001

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "1197f64d-a3f2-4400-a42b-2923ebbc58aa"
-WERK_PATH = "1904-002"
-# READING_WIT = "sfe-1905-008__1905.xml"
+WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
+WERK_PATH = "1901-001"
+READING_WIT = "sfe-1901-001__1925.xml"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
