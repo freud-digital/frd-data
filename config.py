3e1a57a6e5f860a19048085cb0368f2b7cce0377
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-08-11 16:39:26 +0200

    downloaded 1904-006

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "7ed4a279-0d7a-4b4e-b321-365a670506b7"
-WERK_PATH = "1924-005"
-READING_WIT = "sfe-1924-005__1931.xml"
+WERK_ID = "4b6869f3-e463-4498-9fa2-b0a6e55b7882"
+WERK_PATH = "1904-006"
+READING_WIT = "sfe-1904-006__1904.xml"
 
 MANIFEST_DEFAULT_FILTER = {
     "field_doc_component.id": FULL_MANIFEST,
