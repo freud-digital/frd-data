d012897955eb5c67772185790b71154b6c73180e
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-08-29 10:23:28 +0200

    download work 1901-003

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "a0e72083-0ff9-4ae9-bd2a-3bf430ac24a0"
-WERK_PATH = "1905-006"
+WERK_ID = "15142ffb-3fac-4fce-90ba-050e63102267"
+WERK_PATH = "1901-003"
 # READING_WIT = "sfe-1904-006__1904.xml"
 
 MANIFEST_DEFAULT_FILTER = {
