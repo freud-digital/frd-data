b53c3d7154f4d2509adfea492421644918c17096
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-09-20 13:29:18 +0200

    updted api crawler v2.0.5: updated werke

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ HISTORISCHE_AUSGABE = "5b8d9c77-99d0-4a80-92d8-4a9de06ac7ca"
 FRD_USER = os.environ.get('FRD_USER')
 FRD_PW = os.environ.get('FRD_PW')
 
-WERK_ID = "7ed4a279-0d7a-4b4e-b321-365a670506b7"
-WERK_PATH = "1924-005"
+WERK_ID = "2da622da-de12-46e4-bb1d-c9c4a7ae877e"
+WERK_PATH = "1901-001"
 # READING_WIT = "sfe-1904-006__1904.xml"
 
 MANIFEST_DEFAULT_FILTER = {
