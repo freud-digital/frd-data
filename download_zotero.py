c20108c668a1118025bb7de1fb4f353b2de6560c
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-25 09:52:52 +0100

    moved entities files to frd-entities

--- download_zotero.py
+++
@@ -1,6 +0,0 @@
-from zotero_utils import get_listbibl, update_listbibl
-
-list_bibl = "./indices/listbibl.xml"
-
-new_doc = update_listbibl(get_listbibl(limit="50"), orig_file=list_bibl)
-new_doc.tree_to_file(list_bibl)
